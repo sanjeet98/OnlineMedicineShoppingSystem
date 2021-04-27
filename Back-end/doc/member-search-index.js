e298f061bd9864f44f0d7461e10670fb90900b2b
SANJEET KAUL <54444370+sanjeet98@users.noreply.github.com>
2021-04-27 18:37:17 +0530

    Add files via upload

---
+++ Back-end/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.medicineshopping.demo.entity","c":"ProductManagement","l":"ProductManagement()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ Back-end/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.medicineshopping.demo.entity","l":"ProductManagement"}];updateSearchResults();
